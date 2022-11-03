7d8aabd617044651ebb2a2e9b2c8e4c7bc6cab7c
Roar Skinderviken <roar.skinderviken@gmail.com>
2022-11-03 01:01:59 +0100

    Initial commit

---
+++ src/main/kotlin/no/javatec/ViewsController.kt
@@ -0,0 +1,19 @@
+package no.javatec
+
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.views.View
+
+@Controller("/")
+class ViewsController {
+
+    @View("index")
+    @Get
+    fun index(): HttpResponse<Any> = HttpResponse.ok(
+        mapOf(
+            "loggedIn" to true,
+            "username" to "Jon Ole"
+        )
+    )
+}
